e7fc31c950f30fda29c67f65afd2b13060f27742
Keiran <keiran0@proton.me>
2025-04-01 20:45:34 +0100

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import { Root } from "@/components/root";
 export const metadata = {
   title: "AnonHost - Simple, Fast Image Hosting",
   description: "Upload, share, and manage your images with ease.",
+  openGraph: {
+    images: 'https://keiran.cc/a70c08',
+  },
+  metadataBase: new URL('https://keiran.cc'),
 };
 
 export default function RootLayout({

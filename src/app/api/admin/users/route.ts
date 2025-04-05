e412fad0759f9b2d59755a732be344d7017e635a
Keiran <keiran0@proton.me>
2025-04-05 15:06:33 +0200

    unused import

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -1,4 +1,4 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/lib/prisma";

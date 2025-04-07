a803fae93dde36fbda5d8fb34c310e0af0a3a380
Keiran <keiran0@proton.me>
2025-04-07 11:06:53 +0100

    was it really that simple

--- src/lib/sharex.ts
+++ src/lib/sharex.ts
@@ -34,7 +34,7 @@ export const generateShareXShortenerConfig = (
     "Content-Type": "application/json"
   },
   Body: "JSON",
-  Data: JSON.stringify({ originalUrl: "{input}" }),
+  Data: { originalUrl: "{input}" },
   URL: "$json:shortUrl$",
   ErrorMessage: "$json:error$"
 });

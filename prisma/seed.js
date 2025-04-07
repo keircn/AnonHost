717ca0243d0d6faf33a5503137f992a94539c3cd
Keiran <keiran0@proton.me>
2025-04-07 12:01:04 +0100

    fnsjdaghjiuewhn

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,6 +2,15 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
+  await prisma.transaction.deleteMany({
+    where: {
+      type: 'bmc',
+      transactionId: {
+        startsWith: 'BMC_TEST_'
+      }
+    }
+  });
+
   const user = await prisma.user.upsert({
     where: { email: 'keiran0@proton.me' },
     update: {},
@@ -14,7 +23,7 @@ async function main() {
 
   const transaction = await prisma.transaction.create({
     data: {
-      transactionId: 'BMC_TEST_123',
+      transactionId: `BMC_TEST_${Date.now()}`,
       userId: user.id,
       amount: 5.00,
       currency: 'USD',

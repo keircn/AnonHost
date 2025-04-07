939ebb14e0dc0320709a2060eb93272f45d98cb4
Keiran <keiran0@proton.me>
2025-04-07 11:51:48 +0100

    database seed

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: { email: 'keiran0@proton.me' },
+    update: {},
+    create: {
+      email: 'keiran0@proton.me',
+      name: 'q4ow',
+      premium: false,
+      transactions: {
+        create: {
+          transactionId: 'BMC_TEST_123',
+          amount: 5.00,
+          currency: 'USD',
+          type: 'bmc',
+          createdAt: new Date()
+        }
+      }
+    }
+  })
+
+  console.log({ user })
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
